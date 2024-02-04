b3a6e6f303a67c93813bd1e64a8ff3660d883dc6
Bruno Daniel Soares dos Santos <bdsoares@protonmail.com>
2024-02-04 10:11:36 -0300

    Melhorias no footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  contactData: any;
   currentYear: number = new Date().getFullYear();
 
-  constructor() { }
+  constructor(private concactService: ContactService) { }
+
+  ngOnInit() {
+    this.concactService.getContacts().subscribe(data => {
+      this.contactData = data;
+    })
+  }
+
+  removeMailtoPrefix(text: string): string {
+    if (text.startsWith('mailto:')) {
+      return text.substring(7);
+    }
+    return text;
+  }
 }
